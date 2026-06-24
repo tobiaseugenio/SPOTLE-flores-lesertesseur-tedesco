88066b1254387786ec075c2889a87c9136d2931f
Eugeles <eufrales@gmail.com>
2026-06-24 19:46:03 -0300

    primer planteamineto de bdd para las categorias, mejor organizacion en scripts y en los nodos de harmonies

---
+++ scripts/harmonies/categoriasHarmonies.gd
@@ -0,0 +1,9 @@
+extends Node
+class_name CategoriasHarmonies
+ 
+const CATEGORIAS_HARMONIES=[
+	{
+		"nombre": "Canciones de The Beatles",
+		"opciones": ["Yesterday", "Let It Be", "Come Together", "Here Comes The Sun"]
+	}
+]

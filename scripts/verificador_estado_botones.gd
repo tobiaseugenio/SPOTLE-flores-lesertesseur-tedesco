6b8b9234ce1be3bcbcf079f63fb9086ae68de5be
Eugeles <eufrales@gmail.com>
2026-06-24 19:20:49 -0300

    harmonies empezado, se detecta cuando se presiona un boton y cual fue presionado

---
+++ scripts/verificador_estado_botones.gd
@@ -0,0 +1,19 @@
+extends Node
+@onready var botones = $"../GridContainer".get_children()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	conectarBotones()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func conectarBotones():
+	for boton in botones:
+		if boton is Button:
+			boton.pressed.connect(botonPresionado.bind(boton))
+						
+func botonPresionado(boton: Button):
+	print("botonPresionado -> ", boton)
+	

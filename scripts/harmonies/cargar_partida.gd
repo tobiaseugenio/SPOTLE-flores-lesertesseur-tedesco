65c661a74501de465f77d74afcc0cc6f182ccbeb
Eugeles <eufrales@gmail.com>
2026-06-24 20:01:19 -0300

    node cargarPartida planteado, la idea es que ponga las categorias de forma random en la grilla

---
+++ scripts/harmonies/cargar_partida.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var categoriasActivas = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func cargarPartida():
+	var categorias = CategoriasHarmonies.CATEGORIAS_HARMONIES
+	categorias.pick_random()
+	categoriasActivas = categorias

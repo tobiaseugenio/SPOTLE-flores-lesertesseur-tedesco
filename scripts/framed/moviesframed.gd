e370398f9ad2253ea22ad7aa972ec8094d6a4e26
tobiflores <alumno26.flores.tobias@ipm.edu.ar>
2026-06-25 12:17:39 -0300

    hago bdd de framed

---
+++ scripts/framed/moviesframed.gd
@@ -0,0 +1,101 @@
+
+var movies = [
+	{
+		"id": "matrix",
+		"titulo": "The Matrix",
+		"anio": 1999,
+		"alternativas": ["matrix", "the matrix"],
+		"frames": [
+			"res://framed/movies/matrix/frame_1.webp",
+			"res://framed/movies/matrix/frame_2.webp",
+			"res://framed/movies/matrix/frame_3.webp",
+			"res://framed/movies/matrix/frame_4.webp",
+			"res://framed/movies/matrix/frame_5.webp",
+			"res://framed/movies/matrix/frame_6.webp",
+		]
+	},
+	{
+		"id": "tangled",
+		"titulo": "Tangled",
+		"anio": 2010,
+		"alternativas": ["tangled", "enredados", "rapunzel"],
+		"frames": [
+			"res://framed/movies/tangled/frame_1.webp",
+			"res://framed/movies/tangled/frame_2.webp",
+			"res://framed/movies/tangled/frame_3.webp",
+			"res://framed/movies/tangled/frame_4.webp",
+			"res://framed/movies/tangled/frame_5.webp",
+			"res://framed/movies/tangled/frame_6.webp",
+		]
+	},
+	{
+		"id": "et",
+		"titulo": "E.T. the Extra-Terrestrial",
+		"anio": 1982,
+		"alternativas": ["et", "e.t", "extraterrestre"],
+		"frames": [
+			"res://framed/movies/et/frame_1.webp",
+			"res://framed/movies/et/frame_2.webp",
+			"res://framed/movies/et/frame_3.webp",
+			"res://framed/movies/et/frame_4.webp",
+			"res://framed/movies/et/frame_5.webp",
+			"res://framed/movies/et/frame_6.webp",
+		]
+	},
+	{
+		"id": "tiburon",
+		"titulo": "Jaws",
+		"anio": 1975,
+		"alternativas": ["tiburon", "jaws"],
+		"frames": [
+			"res://framed/movies/tiburon/frame_1.webp",
+			"res://framed/movies/tiburon/frame_2.webp",
+			"res://framed/movies/tiburon/frame_3.webp",
+			"res://framed/movies/tiburon/frame_4.webp",
+			"res://framed/movies/tiburon/frame_5.webp",
+			"res://framed/movies/tiburon/frame_6.webp",
+		]
+	},
+	{
+		"id": "el_padrino",
+		"titulo": "The Godfather",
+		"anio": 1972,
+		"alternativas": ["el padrino", "the godfather", "padrino"],
+		"frames": [
+			"res://framed/movies/el_padrino/frame_1.webp",
+			"res://framed/movies/el_padrino/frame_2.webp",
+			"res://framed/movies/el_padrino/frame_3.webp",
+			"res://framed/movies/el_padrino/frame_4.webp",
+			"res://framed/movies/el_padrino/frame_5.webp",
+			"res://framed/movies/el_padrino/frame_6.webp",
+		]
+	},
+	{
+		"id": "titanic",
+		"titulo": "Titanic",
+		"anio": 1997,
+		"alternativas": ["titanic"],
+		"frames": [
+			"res://framed/movies/titanic/frame_1.webp",
+			"res://framed/movies/titanic/frame_2.webp",
+			"res://framed/movies/titanic/frame_3.webp",
+			"res://framed/movies/titanic/frame_4.webp",
+			"res://framed/movies/titanic/frame_5.webp",
+			"res://framed/movies/titanic/frame_6.webp",
+		]
+	},
+	{
+		"id": "pulp_fiction",
+		"titulo": "Pulp Fiction",
+		"anio": 1994,
+		"alternativas": ["pulp fiction", "pulpfiction"],
+		"frames": [
+			"res://framed/movies/pulp_fiction/frame_1.webp",
+			"res://framed/movies/pulp_fiction/frame_2.webp",
+			"res://framed/movies/pulp_fiction/frame_3.webp",
+			"res://framed/movies/pulp_fiction/frame_4.webp",
+			"res://framed/movies/pulp_fiction/frame_5.webp",
+			"res://framed/movies/pulp_fiction/frame_6.webp",
+		]
+	},
+]

4d4743e6c6a11bc63c955ea27564de44b0e15bff
tobiflores <alumno26.flores.tobias@ipm.edu.ar>
2026-06-26 11:32:04 -0300

    Implemente buscador de peliculas, hice funcional las fotos, corregi sintaxis del script para que sea jugable, arranco a agregar fotos a todas las pelis ahora q se que funciona

--- scripts/framed/framed.gd
+++ scripts/framed/framed.gd
@@ -4,27 +4,42 @@ var pelicula_actual: Dictionary
 var frame_actual := 0
 
 func _ready():
+	for i in range(6):
+		var boton = $ContenedorBotones.get_child(i)
+		boton.pressed.connect(_on_boton_frame_pressed.bind(i))
 	nueva_partida()
 
+func _on_boton_frame_pressed(indice: int):
+	frame_actual = indice
+	mostrar_frame()
+
 func nueva_partida():
-	pelicula_actual = FramesPelis.peliculas.pick_random()
+	pelicula_actual = Moviesframed.movies.pick_random()
 	frame_actual = 0
 	mostrar_frame()
+	actualizar_botones()
 
 func mostrar_frame():
 	var ruta = pelicula_actual["frames"][frame_actual]
-	$TextureRect.texture = load(ruta)
+	print(ruta)
+	$Foto.texture = load(ruta)
+
+func actualizar_botones():
+	for i in range(6):
+		var boton = $ContenedorBotones.get_child(i)
+		boton.disabled = i > frame_actual
 
 func revelar_siguiente():
 	if frame_actual < 5:
 		frame_actual += 1
 		mostrar_frame()
+		actualizar_botones()
 	else:
 		juego_terminado(false)
 
 func validar_respuesta(texto: String):
 	var intento = texto.strip_edges().to_lower()
-	if intento in pelicula_actual["respuestas"]:
+	if intento in pelicula_actual["alternativas"]:
 		juego_terminado(true)
 	else:
 		revelar_siguiente()
@@ -35,27 +50,29 @@ func juego_terminado(gano: bool):
 	else:
 		print("Perdiste - era: ", pelicula_actual["titulo"])
 		
-func _on_line_edit_text_changed(text):
-	actualizar_sugerencias(text)
 
-func actualizar_sugerencias(texto):
+func actualizar_sugerencias(texto: String):
 	$ItemList.clear()
-	
 	if texto == "":
 		$ItemList.visible = false
 		return
 	var hay_resultados := false
-
-	for peli in FramesPelis.peliculas:
+	for peli in Moviesframed.movies:
 		if peli["titulo"].to_lower().contains(texto.to_lower()):
 			$ItemList.add_item(peli["titulo"])
 			hay_resultados = true
 	$ItemList.visible = hay_resultados
-	
-func _on_item_list_item_clicked(index, at_position, mouse_button_index):
-	var titulo = $ItemList.get_item_text(index)
-	validar_respuesta(titulo)
+
+func _on_line_escribir_text_submitted(texto: String) -> void:
+	$ItemList.visible = false
+	validar_respuesta(texto)
+	$LineEscribir.clear()
 
 func _on_line_escribir_text_changed(texto: String) -> void:
-	$ItemList.visible = texto != ""
 	actualizar_sugerencias(texto)
+
+func _on_item_list_item_selected(index: int) -> void:
+	var titulo = $ItemList.get_item_text(index)
+	$ItemList.visible = false
+	$LineEscribir.clear()
+	validar_respuesta(titulo)
--- scripts/framed/moviesframed.gd
+++ scripts/framed/moviesframed.gd
@@ -1,3 +1,4 @@
+extends Node
 
 var movies = [
 	{
@@ -6,12 +7,12 @@ var movies = [
 		"anio": 1999,
 		"alternativas": ["matrix", "the matrix"],
 		"frames": [
-			"res://framed/fotos/matrix/frame1.png",
-			"res://framed/fotos/matrix/frame2.png",
-			"res://framed/fotos/matrix/frame3.png",
-			"res://framed/fotos/matrix/frame4.png",
-			"res://framed/fotos/matrix/frame5.png",
-			"res://framed/fotos/matrix/frame6.png",
+			"res://fotos/matrix/frame1.png",
+			"res://fotos/matrix/frame2.png",
+			"res://fotos/matrix/frame3.png",
+			"res://fotos/matrix/frame4.png",
+			"res://fotos/matrix/frame5.png",
+			"res://fotos/matrix/frame6.png",
 		]
 	},
 	{
@@ -20,12 +21,12 @@ var movies = [
 		"anio": 2010,
 		"alternativas": ["tangled", "enredados", "rapunzel"],
 		"frames": [
-			"res://framed/fotos/tangled/frame1.png",
-			"res://framed/fotos/tangled/frame2.png",
-			"res://framed/fotos/tangled/frame3.png",
-			"res://framed/fotos/tangled/frame4.png",
-			"res://framed/fotos/tangled/frame5.png",
-			"res://framed/fotos/tangled/frame6.png",
+			"res://fotos/tangled/frame1.png",
+			"res://fotos/tangled/frame2.png",
+			"res://fotos/tangled/frame3.png",
+			"res://fotos/tangled/frame4.png",
+			"res://fotos/tangled/frame5.png",
+			"res://fotos/tangled/frame6.png",
 		]
 	},
 	{
@@ -34,12 +35,12 @@ var movies = [
 		"anio": 1982,
 		"alternativas": ["et", "e.t", "extraterrestre"],
 		"frames": [
-			"res://framed/fotos/et/frame1.png",
-			"res://framed/fotos/et/frame2.png",
-			"res://framed/fotos/et/frame3.png",
-			"res://framed/fotos/et/frame4.png",
-			"res://framed/fotos/et/frame5.png",
-			"res://framed/fotos/et/frame6.png",
+			"res://fotos/et/frame1.png",
+			"res://fotos/et/frame2.png",
+			"res://fotos/et/frame3.png",
+			"res://fotos/et/frame4.png",
+			"res://fotos/et/frame5.png",
+			"res://fotos/et/frame6.png",
 		]
 	},
 	{
@@ -48,12 +49,12 @@ var movies = [
 		"anio": 1975,
 		"alternativas": ["tiburon", "jaws"],
 		"frames": [
-			"res://framed/fotos/tiburon/frame1.png",
-			"res://framed/fotos/tiburon/frame2.png",
-			"res://framed/fotos/tiburon/frame3.png",
-			"res://framed/fotos/tiburon/frame4.png",
-			"res://framed/fotos/tiburon/frame5.png",
-			"res://framed/fotos/tiburon/frame6.png",
+			"res://fotos/tiburon/frame1.png",
+			"res://fotos/tiburon/frame2.png",
+			"res://fotos/tiburon/frame3.png",
+			"res://fotos/tiburon/frame4.png",
+			"res://fotos/tiburon/frame5.png",
+			"res://fotos/tiburon/frame6.png",
 		]
 	},
 	{
@@ -62,12 +63,12 @@ var movies = [
 		"anio": 1972,
 		"alternativas": ["el padrino", "the godfather", "padrino"],
 		"frames": [
-			"res://framed/fotos/el_padrino/frame1.png",
-			"res://framed/fotos/el_padrino/frame2.png",
-			"res://framed/fotos/el_padrino/frame3.png",
-			"res://framed/fotos/el_padrino/frame4.png",
-			"res://framed/fotos/el_padrino/frame5.png",
-			"res://framed/fotos/el_padrino/frame6.png",
+			"res://fotos/el_padrino/frame1.png",
+			"res://fotos/el_padrino/frame2.png",
+			"res://fotos/el_padrino/frame3.png",
+			"res://fotos/el_padrino/fsssrame4.png",
+			"res://fotos/el_padrino/frame5.png",
+			"res://fotos/el_padrino/frame6.png",
 		]
 	},
 	{
@@ -76,12 +77,12 @@ var movies = [
 		"anio": 1997,
 		"alternativas": ["titanic"],
 		"frames": [
-			"res://framed/fotos/titanic/frame1.png",
-			"res://framed/fotos/titanic/frame2.png",
-			"res://framed/fotos/titanic/frame3.png",
-			"res://framed/fotos/titanic/frame4.png",
-			"res://framed/fotos/titanic/frame5.png",
-			"res://framed/fotos/titanic/frame6.png",
+			"res://fotos/titanic/frame1.png",
+			"res://fotos/titanic/frame2.png",
+			"res://fotos/titanic/frame3.png",
+			"res://fotos/titanic/frame4.png",
+			"res://fotos/titanic/frame5.png",
+			"res://fotos/titanic/frame6.png",
 		]
 	},
 	{
@@ -90,12 +91,12 @@ var movies = [
 		"anio": 1994,
 		"alternativas": ["pulp fiction", "pulpfiction"],
 		"frames": [
-			"res://framed/fotos/pulp_fiction/frame1.png",
-			"res://framed/fotos/pulp_fiction/frame2.png",
-			"res://framed/fotos/pulp_fiction/frame3.png",
-			"res://framed/fotos/pulp_fiction/frame4.png",
-			"res://framed/fotos/pulp_fiction/frame5.png",
-			"res://framed/fotos/pulp_fiction/frame6.png",
+			"res://fotos/pulp_fiction/frame1.png",
+			"res://fotos/pulp_fiction/frame2.png",
+			"res://fotos/pulp_fiction/frame3.png",
+			"res://fotos/pulp_fiction/frame4.png",
+			"res://fotos/pulp_fiction/frame5.png",
+			"res://fotos/pulp_fiction/frame6.png",
 		]
 	},
 		{
@@ -104,12 +105,12 @@ var movies = [
 		"anio": 2023,
 		"alternativas": ["elemental","elementos"],
 		"frames": [
-			"res://framed/fotos/elemental/frame1.png",
-			"res://framed/fotos/elemental/frame2.png",
-			"res://framed/fotos/elemental/frame3.png",
-			"res://framed/fotos/elemental/frame4.png",
-			"res://framed/fotos/elemental/frame5.png",
-			"res://framed/fotos/elemental/frame6.png",
+			"res://fotos/elemental/frame1.png",
+			"res://fotos/elemental/frame2.png",
+			"res://fotos/elemental/frame3.png",
+			"res://fotos/elemental/frame4.png",
+			"res://fotos/elemental/frame5.png",
+			"res://fotos/elemental/frame6.png",
 		]
 	},
 ]

9321e306fcf8d0a6b6521e0b3620cba954fadcf3
torialfaro <ALUMNO26.ALFARO.Victoria@ipm.edu.ar>
2026-06-29 17:40:27 -0300

    primer planteamiento del estado de los botones

--- scripts/harmonies/conectar_botones.gd
+++ scripts/harmonies/conectar_botones.gd
@@ -1,10 +1,10 @@
 extends Node
 @onready var botones = $"../GridContainer".get_children()
-
+@onready var verificadorEstadoBotones = $"../VerificadorEstadoBotones"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	conectarBotones()
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
@@ -12,8 +12,10 @@ func _process(delta: float) -> void:
 func conectarBotones():
 	for boton in botones:
 		if boton is Button:
-			boton.pressed.connect(botonPresionado.bind(boton))
-						
-func botonPresionado(boton: Button):
-	print("botonPresionado -> ", boton)
-	
+			#boton.pressed.connect(verificadorEstadoBotones.botonPresionado)
+			verificadorEstadoBotones.botonPresionado(boton)
+			#boton.pressed.connect(verificadorEstadoBotones.botonPresionado(boton).bind(boton))	
+
+
+#func botonPresionado(boton: Button):
+	#print("botonPresionado -> ", boton)
--- scripts/harmonies/verificador_estado_botones.gd
+++ scripts/harmonies/verificador_estado_botones.gd
@@ -1,5 +1,6 @@
 extends Node
 @onready var botones = $"../GridContainer".get_children()
+var opcionesElegidas= []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,3 +9,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func botonPresionado(boton: Button):
+	#print("verificadorEstado botonPresionado -> ", boton)
+	opcionesElegidas.append(boton)
+	mostrarOpcionesElegidas()
+
+func mostrarOpcionesElegidas():
+	for opcion in opcionesElegidas:
+		print("mostrarOpcionesElegidas() -> ", opcion)
+	
+	
+	

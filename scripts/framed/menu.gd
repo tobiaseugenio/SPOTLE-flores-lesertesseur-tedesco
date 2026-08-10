61f86f3183b855b90eac82d9442ab1518b111467
Eugeles <eufrales@gmail.com>
2026-08-10 16:56:58 -0300

    sfx para botones de framed y spotle

--- scripts/framed/menu.gd
+++ scripts/framed/menu.gd
@@ -1,9 +1,19 @@
 extends Control
 
 func _on_pelis_boton_pressed() -> void:
+	$aceptar.play()
+	await $aceptar.finished
 	ModosDeJuego.current_mode = ModosDeJuego.ModosDeJuego.MOVIES
 	get_tree().change_scene_to_file("res://scenes/framed/framed.tscn")
 
 func _on_videoclips_boton_pressed() -> void:
+	$aceptar.play()
+	await $aceptar.finished
 	ModosDeJuego.current_mode = ModosDeJuego.ModosDeJuego.VIDEOCLIPS
 	get_tree().change_scene_to_file("res://scenes/framed/framed.tscn")
+
+func _on_videoclips_button_mouse_entered() -> void:
+	$hover.play()
+
+func _on_pelis_button_mouse_entered() -> void:
+	$hover.play()

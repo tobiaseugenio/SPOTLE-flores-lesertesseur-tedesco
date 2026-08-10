c0882d37495b11c74b7b3cba09f84c75d5ed68cd
Eugeles <eufrales@gmail.com>
2026-08-10 17:29:08 -0300

    fix para transicion de explicacion spotke que no se ocultaba

--- scenes/spotle/explicacion_spotle.gd
+++ scenes/spotle/explicacion_spotle.gd
@@ -1,4 +1,4 @@
-extends Button
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,3 +9,13 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_btn_volver_pressed() -> void:
+	$aceptar.play()
+	await $aceptar.finished
+	get_tree().change_scene_to_file("res://scenes/spotle/decidirSpotle.tscn")
+
+
+func _on_btn_volver_mouse_entered() -> void:
+	$hover.play()
